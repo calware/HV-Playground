382a0065405b4a288b8dcc2909ee88a5f78b1ee6
calware <47096877+calware@users.noreply.github.com>
2020-08-28 14:01:40 -0700

    Removed unnecessary project files

--- scripts/Revert.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-cd C:\Program Files (x86)\VMware\VMware Workstation
-vmrun.exe revertToSnapshot "C:\Users\cal\Desktop\z\vms\w10_kdev_chk\Windows 10 x64 Kernel Debug [Checked]\Windows 10 x64 Kernel Debug [Checked].vmx" RocketDriver_v1_vmx_dbg
-vmrun.exe start "C:\Users\cal\Desktop\z\vms\w10_kdev_chk\Windows 10 x64 Kernel Debug [Checked]\Windows 10 x64 Kernel Debug [Checked].vmx"
--- scripts/UploadExecute.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-curl http://192.168.110.128:25090/install -F "file=@./x64/Debug/SPTHv.sys" -F "execute=true" -vvv
